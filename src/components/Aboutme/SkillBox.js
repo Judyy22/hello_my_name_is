15d46ec6dff51ee38e38fdf06087152c68c8a20d
Seunghui Lee <115631038+Judyy22@users.noreply.github.com>
2023-02-22 22:13:37 +0900

    skill 망함

--- src/components/Aboutme/SkillBox.js
+++ src/components/Aboutme/SkillBox.js
@@ -11,34 +11,37 @@ const SkillBox = (props) => {
     console.log("menuactive", menuActive);
 
     return (
-        <div className="box">
-            <div className="mobilename">
-                <div className="box-name">{name}</div>
-                <div
-                    onClick={() => setMenuActive(!menuActive)}
-                    className="bars"
-                >
-                    {menuActive ? (
-                        <FontAwesomeIcon icon={faXmark} className="xmark" />
-                    ) : (
-                        <FontAwesomeIcon icon={faBars} />
-                    )}
+        <div className="box container">
+            <div className="item front">
+                <div className="mobilename">
+                    <div className="box-name">{name}</div>
+                    <div
+                        onClick={() => setMenuActive(!menuActive)}
+                        className="bars"
+                    >
+                        {menuActive ? (
+                            <FontAwesomeIcon icon={faXmark} className="xmark" />
+                        ) : (
+                            <FontAwesomeIcon icon={faBars} />
+                        )}
+                    </div>
+                </div>
+                <div className="img-box">
+                    {image.map((item) => {
+                        return (
+                            <img src={`pictures/${item}.png`} width="60vw" />
+                        );
+                    })}
+                </div>
+                <div className={menuActive ? "img-box-mobile" : "img-box-none"}>
+                    {image.map((item) => {
+                        return (
+                            <img src={`pictures/${item}.png`} width="60vw" />
+                        );
+                    })}
                 </div>
             </div>
-            <div className="img-box">
-                {image.map((item) => {
-                    return <img src={`pictures/${item}.png`} width="60px" />;
-                })}
-            </div>
-            <div className="img-box-mobile">
-                {menuActive
-                    ? image.map((item) => {
-                          return (
-                              <img src={`pictures/${item}.png`} width="60px" />
-                          );
-                      })
-                    : ""}
-            </div>
+            <div className="item back">뒷면</div>
         </div>
     );
 };
