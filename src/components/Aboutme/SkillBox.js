ffa9ebca223cbaa8c2101e209f6a6149804d3d03
Seunghui Lee <115631038+Judyy22@users.noreply.github.com>
2023-02-22 17:06:21 +0900

    skill 수정중임

--- src/components/Aboutme/SkillBox.js
+++ src/components/Aboutme/SkillBox.js
@@ -1,17 +1,43 @@
-import React from "react";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
 import "./SkillBox.css";
 
 const SkillBox = (props) => {
     const name = props.name;
     const image = props.picture;
+    const [menuActive, setMenuActive] = useState(false);
+
+    console.log("menuactive", menuActive);
 
     return (
         <div className="box">
-            <div className="box-name">{name}</div>
+            <div className="mobilename">
+                <div className="box-name">{name}</div>
+                <div
+                    onClick={() => setMenuActive(!menuActive)}
+                    className="bars"
+                >
+                    {menuActive ? (
+                        <FontAwesomeIcon icon={faXmark} className="xmark" />
+                    ) : (
+                        <FontAwesomeIcon icon={faBars} />
+                    )}
+                </div>
+            </div>
             <div className="img-box">
                 {image.map((item) => {
                     return <img src={`pictures/${item}.png`} width="60px" />;
                 })}
+            </div>
+            <div className="img-box-mobile">
+                {menuActive
+                    ? image.map((item) => {
+                          return (
+                              <img src={`pictures/${item}.png`} width="60px" />
+                          );
+                      })
+                    : ""}
             </div>
         </div>
     );
--- src/components/Aboutme/Skills.js
+++ src/components/Aboutme/Skills.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SkillBox from "./SkillBox";
+import "./Skills.css";
 
 const Skills = () => {
     const frontpic = ["html", "css", "javascript", "bootstrap", "react"];
@@ -8,12 +9,14 @@ const Skills = () => {
     const workspace = ["vscode", "eclipse", "developer", "workbench"];
     const design = ["illustrator", "figma"];
     return (
-        <div>
-            <SkillBox name="Front-End" picture={frontpic} />
-            {/* <SkillBox name="Back-End" picture={backpic} />
-            <SkillBox name="DB" picture={dbpic} />
-            <SkillBox name="WorkSpace" picture={workspace} />
-            <SkillBox name="Design Tool" picture={design} /> */}
+        <div className="allskills">
+            <div className="skills">
+                <SkillBox name="Front-End" picture={frontpic} />
+                <SkillBox name="Back-End" picture={backpic} />
+                <SkillBox name="DB" picture={dbpic} />
+                <SkillBox name="WorkSpace" picture={workspace} />
+                <SkillBox name="Design Tool" picture={design} />
+            </div>
         </div>
     );
 };
