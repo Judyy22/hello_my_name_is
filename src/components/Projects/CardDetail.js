999a016abe68d007858c440e382a87be6600b060
Judyy22 <115631038+Judyy22@users.noreply.github.com>
2023-02-27 19:36:06 +0900

    어우 뭔가 이상해짐..ㅠㅠ

--- src/components/Projects/CardDetail.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Card from "react-bootstrap/Card";
-
-const CardDetail = (props) => {
-    // console.log(props);
-    return <div>메롱</div>;
-};
-
-export default CardDetail;
